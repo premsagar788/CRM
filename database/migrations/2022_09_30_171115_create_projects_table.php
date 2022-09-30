90f8ea6fe2276aa0af55457169d0c10a193c7d66
Prem Sagar <86755890+premsagarnijar@users.noreply.github.com>
2022-09-30 22:23:41 +0500

    created_projects_table

---
+++ database/migrations/2022_09_30_171115_create_projects_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('client')->unsigned();
+            $table->unsignedSmallInteger('phone')->unsigned();
+            $table->enum('billing_type', ['Attempted', 'Fixed Rate', 'Project Hours', 'Task Hours']);
+            $table->string('estimated_time', 11);
+            $table->date('start_date');
+            $table->date('deadline');
+            $table->longText('description');
+
+            $table->foreign('client')->references('id')->on('clients');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
