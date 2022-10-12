01249bb1b7b337b3db9cc83d41f8c715a055a14c
Prem Sagar <prem_saagar@ymail.com>
2022-10-12 19:04:53 +0500

    improved dashboard statistics

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lead;
+use App\Models\Client;
+
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+    	$leads = Lead::all();
+    	$clients = Client::all();
+        return view('admin.dashboard')->with(compact('leads', 'clients'));
     }
 }
