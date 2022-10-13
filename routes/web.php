2106eda7d1c74444c1465d8b8adfbf2ecca413f9
Prem Sagar <prem_saagar@ymail.com>
2022-10-13 20:10:09 +0500

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -43,8 +43,8 @@ Route::controller(App\Http\Controllers\LeadsController::class)->middleware('auth
 
 Route::controller(App\Http\Controllers\ProjectsController::class)->middleware('auth')->group(function () {
     Route::get('/admin/projects', 'index');
-    Route::get('/admin/projects/add', 'addLead');
-    Route::post('/admin/projects/add', 'addLeadSave');
+    Route::get('/admin/projects/add', 'addProject');
+    Route::post('/admin/projects/add', 'addProjectSave');
     Route::get('/admin/projects/edit/{id}', 'editLeadView');
     Route::post('/admin/projects/update/{id}', 'editLeadSave');
     Route::get('/admin/projects/delete/{id}', 'deleteLead');
