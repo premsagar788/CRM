cc63e81e1f7b6d69aaf2aaa57a0b2814310f9da6
Prem Sagar <50700219+premsagar788@users.noreply.github.com>
2022-10-19 23:59:43 +0500

    API (#24)
    * updated Readme
    
    * Views (#3)
    
    * Update README.md
    
    * update
    
    Co-authored-by: Prem Sagar <86755890+premsagarnijar@users.noreply.github.com>
    
    * Update README.md
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Update addclient.blade.php
    
    * add client css
    
    * addclient css
    
    * Update sb-admin-2.css
    
    * improved spacing
    
    * improved routing
    
    * update
    
    * update
    
    * Readme update
    
    * Readme update
    
    * updated leads page
    
    * updated add lead page
    
    * Fixed Lead update
    
    * updated request class
    
    * Updated leads addition
    
    * Update AddLeadRequest.php
    
    * update
    
    * security issue fixed
    
    * security issue fixed
    
    * security issue fixed
    
    * Readme update
    
    * update client
    
    * update lead
    
    * update client
    
    * updated leads view
    
    * updated leads view
    
    * update client
    
    * added routes for edit/update client and lead
    
    * Readme update
    
    * Readme update
    
    * Update README.md
    
    * Readme update
    
    * Readme update
    
    * delete client
    
    * delete Lead
    
    * code style fix
    
    * Readme update
    
    * improved notification text
    
    * added routes for delete client and lead
    
    * update lead fix
    
    * update lead fix
    
    * improved dashboard statistics
    
    * update
    
    * updated projects table
    
    * update project model
    
    * update layout add projects tab
    
    * add projects routes
    
    * view projects
    
    * updated routes
    
    * removed request class
    
    * update
    
    * update
    
    * Update addproject.blade.php
    
    * Update LeadsController.php
    
    * update
    
    * update
    
    * update
    
    * Update web.php
    
    * update
    
    * update
    
    * update
    
    * update
    
    * frontend update
    
    * api routes
    
    * api controllers
    
    Signed-off-by: Prem Sagar <50700219+premsagar788@users.noreply.github.com>
    Co-authored-by: Prem Sagar <86755890+premsagarnijar@users.noreply.github.com>

---
+++ app/Http/Controllers/LeadsAPIController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use \Exception;
+use App\Models\Lead;
+use App\Http\Requests\AddLeadRequest;
+use App\Http\Requests\UpdateLeadRequest;
+
+class LeadsAPIController extends Controller
+{
+    public function index()
+    {
+    	$leads = Lead::all();
+    	return view('admin.leads')->with(compact('leads'));
+    }
+
+    public function addLead()
+    {
+    	return view('admin.addlead');
+    }
+
+    public function addLeadSave(AddLeadRequest $request)
+    {
+    	$data = $request->validated();
+
+        try {
+            $lead = new Lead();
+            $lead->name       	= $data['name']; 
+            $lead->company_name	= $data['company_name'];
+            $lead->source      	= $data['source'];
+            $lead->budget     	= $data['budget'];
+            $lead->website     	= $data['website'];
+            $lead->phone      	= $data['phone'];
+            $lead->country    	= $data['country'];
+            $lead->description 	= $data['description'];
+            $lead->status       = $data['status'];
+            $lead->save();
+
+            return redirect('/admin/leads')->with('success', 'Lead added successfully!');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('errors', $e->getMessage());
+        }
+    }
+
+    public function editLeadView($id)
+    {
+        $lead = Lead::findOrFail($id);
+        return view('admin.update-lead')->with(compact('lead'));
+    }
+
+    public function editLeadSave(UpdateLeadRequest $request, $id)
+    {
+        $data = $request->validated();
+
+        $lead = Lead::findOrFail($id);
+
+        $lead->name         = $data['name']; 
+        $lead->company_name = $data['company_name'];
+        $lead->source       = $data['source'];
+        $lead->budget       = $data['budget'];
+        $lead->website      = $data['website'];
+        $lead->phone        = $data['phone'];
+        $lead->country      = $data['country'];
+        $lead->description  = $data['description'];
+        $lead->status       = $data['status'];
+        $lead->save();
+        return redirect('/admin/leads')->with('success', 'Lead updated successfully!');
+    }
+
+    public function deleteLead($id)
+    {
+        $lead = Lead::findOrFail($id);
+        $lead->delete();
+        return redirect('/admin/leads')->with('success', 'Lead deleted successfully!');
+    }
+    
+}
---
+++ app/Http/Controllers/ProjectsAPIController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use \Exception;
+use App\Models\Client;
+use App\Models\Project;
+use App\Http\Requests\AddProjectRequest;
+
+class ProjectsAPIController extends Controller
+{
+    public function index()
+    {
+    	$projects = Project::all()->pluck('name');
+    	return view('admin.projects')->with(compact('projects'));
+    }
+
+    public function addProject()
+    {
+    	$clients = Client::select('name', 'id')->get();
+    	return view('admin.addproject')->with(compact('clients'));
+    }
+
+    public function addProjectSave(AddProjectRequest $request)
+    {
+    	$data = $request->validated();
+
+        try {
+            $project = new Project();
+            $project->name       		= $data['name']; 
+            $project->client			= $data['client'];
+            $project->phone      		= $data['phone'];
+            $project->billing_type		= $data['billing_type'];
+            $project->status            = $data['status'];
+            $project->estimated_time	= $data['estimated_time'];
+            $project->start_date     	= $data['start_date'];
+            $project->deadline    		= $data['deadline'];
+            $project->description 		= $data['description'];
+            $project->save();
+            return redirect('/admin/projects')->with('success', 'Project added successfully!');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('errors', $e->getMessage());
+        }
+    }
+
+    public function editProjectView($id)
+    {
+        $project = Project::findOrFail($id);
+        return view('admin.update-project')->with(compact('project'));
+    }
+
+    public function editProjectSave(UpdateLeadRequest $request, $id)
+    {
+        $data = $request->validated();
+
+        $project = Project::findOrFail($id);
+
+        $project->name         = $data['name']; 
+        $project->company_name = $data['company_name'];
+        $project->source       = $data['source'];
+        $project->budget       = $data['budget'];
+        $project->website      = $data['website'];
+        $project->phone        = $data['phone'];
+        $project->country      = $data['country'];
+        $project->description  = $data['description'];
+        $project->status       = $data['status'];
+        $project->save();
+        return redirect('/admin/leads')->with('success', 'Project updated successfully!');
+    }
+
+    public function deleteLead($id)
+    {
+        $project = Project::findOrFail($id);
+        $project->delete();
+        return redirect('/admin/leads')->with('success', 'Project deleted successfully!');
+    }
+}
