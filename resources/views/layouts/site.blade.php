e2012d092cd775adb4a7d6504ec225c5b96a5320
Prem Sagar <prem_saagar@ymail.com>
2023-03-18 14:33:09 +0500

    js fixes

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -147,7 +147,7 @@
       </footer>
     </main>
 
-    <script src="{{ asset('js/bootstrap.bundle.min.js') }}" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 
   </body>
 </html>
